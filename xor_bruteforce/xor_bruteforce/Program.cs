8f0ba303bd53855da4c3b4bd137de371d308ad51
Besenyei Márk <besmark@t-onlne.hu>
2017-03-06 18:00:08 +0100

    asd

---
+++ xor_bruteforce/xor_bruteforce/Program.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+
+
+namespace xor_bruteforce
+{
+    class Program
+    {
+        //szavak az ellenőruéshez
+        static string[] szavak = { "feladat", "szöveg", };
+
+        static string kodoltszoveg;
+
+        public static int probalkozas = 1;
+
+        public static bool megoldva = false;
+
+        static Thread core1 = new Thread(new ThreadStart(Core1));
+        static Thread core2 = new Thread(new ThreadStart(Core2));
+        static Thread core3 = new Thread(new ThreadStart(Core3));
+        static Thread core4 = new Thread(new ThreadStart(Core4));
+
+        //beállítások
+        static int minhossz = 0;
+        static int maxhossz = 10;
+        // 1 = igen | 2 = nem
+        static int lehet_nagybetu = 0;
+        static int lehet_kisbetu = 0;
+        static int lehet_szam = 1;
+        static int lehet_special = 0;
+
+        static string input = "";
+        static string output = "";
+
+        //készít egy kódolt szöveget
+        static string feladatkeszites()
+        {
+            string text;
+            text = exor("A titkositt szöveges feladat", "1234567");
+
+            return text;
+        }
+
+        //static void Main(string[] args)
+        static void Main(string[] args)
+        {
+            if (args.Length != 0)
+            {
+                if (args.Length > 0)
+                    input = args[0];
+                if (args.Length > 1)
+                    output = args[1];
+            }
+            //minhossz beállítása
+            core1prog.minhossz(minhossz);
+            core2prog.minhossz(minhossz);
+            core3prog.minhossz(minhossz);
+            core4prog.minhossz(minhossz);
+
+            //feladatkészítés hívása
+            //kodoltszoveg = feladatkeszites();
+
+            //feladat beolvasása
+            string szoveg = System.IO.File.ReadAllText(input);
+            kodoltszoveg = exor(szoveg, "123");
+            Console.WriteLine(kodoltszoveg);
+            Console.ReadKey();
+
+            //kiírás fájlba
+            //System.IO.File.WriteAllLines(output, szoveg);
+
+            //multithread indítása
+            /*
+            core1.Start();
+            core2.Start();
+            core3.Start();
+            core4.Start();
+            */
+
+
+
+        }
+
+        //exor 
+        static string exor(string text, string key)
+        {
+            char[] output = new char[text.Length];
+
+            for (int i = 0; i < text.Length; i++)
+                output[i] = (char)(text[i] ^ key[i % key.Length]);
+
+            return new string(output);
+        }
+
+        //dedolt szöveg tesztelése
+        static void teszt(string text, string key)
+        {
+            //tartalmaz-e megadott szavakat
+            if (szavak.Any(text.Contains))
+            {
+                Console.WriteLine("Megoldás: " + text);
+                Console.WriteLine("Próba: " + probalkozas);
+                Console.WriteLine("Kulcs: " + key);
+            }
+            //vagy szóhosszúság ellenőrzése
+            if (text.Contains(' '))
+            {
+                int space = text.Split(' ').Length - 1;
+                int szoveghossz = text.Length;
+                int szohossz = szoveghossz / space;
+                if (szohossz > 6 && szohossz < 9
+                    && text.Contains("hogy")
+                    && text.Contains("az")
+                    && text.Contains("nem")
+                    && text.Contains("ha"))
+                {
+                    Console.WriteLine("Megoldás: " + text);
+                    Console.WriteLine("Próba: " + probalkozas);
+                    Console.WriteLine("Kulcs: " + key);
+                }
+            }
+        }
+
+        
+        static void Core1()
+        {
+            //checkpoint az időnkénti kiíratáshoz
+            int checkpoint = 0;
+            //megadja a kulcs karakterkészletét
+            core1prog.arraybuilder(lehet_nagybetu, lehet_kisbetu, lehet_szam, lehet_special, 0);
+            string kulcs;
+            //páratlan számú kulcs miatt 1x
+            kulcs = core1prog.strconst(maxhossz);
+            while (megoldva == false)
+            {
+                //2x generál kulcsot hogy páratlan maradjon
+                kulcs = core1prog.strconst(maxhossz);
+                kulcs = core1prog.strconst(maxhossz);
+                //időnkénti kiíratás
+                if (probalkozas > checkpoint + 20000000)
+                {
+                    Console.WriteLine("Próba: " + probalkozas);
+                    Console.WriteLine("Kulcs: " + kulcs);
+                    checkpoint = probalkozas;
+                }
+                probalkozas++;
+
+                //szöveg tesztelése
+                teszt(exor(kodoltszoveg, kulcs), kulcs);
+            }
+        }
+        static void Core2()
+        {
+
+            core2prog.arraybuilder(lehet_nagybetu, lehet_kisbetu, lehet_szam, lehet_special, 0);
+            string kulcs;
+            //2x hogy páros legyen
+            kulcs = core2prog.strconst(maxhossz);
+            kulcs = core2prog.strconst(maxhossz);
+            while (megoldva == false)
+            {
+                //2x hogy páros maradjon
+                kulcs = core2prog.strconst(maxhossz);
+                kulcs = core2prog.strconst(maxhossz);
+
+                probalkozas++;
+
+                teszt(exor(kodoltszoveg, kulcs), kulcs);
+            }
+        }
+        static void Core3()
+        {
+            core3prog.arraybuilder(lehet_nagybetu, lehet_kisbetu, lehet_szam, lehet_special, 1);
+            string kulcs;
+            kulcs = core3prog.strconst(maxhossz);
+            while (megoldva == false)
+            {
+                kulcs = core3prog.strconst(maxhossz);
+                kulcs = core3prog.strconst(maxhossz);
+
+                probalkozas++;
+
+                teszt(exor(kodoltszoveg, kulcs), kulcs);
+            }
+        }
+        static void Core4()
+        {
+            core4prog.arraybuilder(lehet_nagybetu, lehet_kisbetu, lehet_szam, lehet_special, 1);
+            string kulcs;
+            kulcs = core4prog.strconst(maxhossz);
+            kulcs = core4prog.strconst(maxhossz);
+            while (megoldva == false)
+            {
+                kulcs = core4prog.strconst(maxhossz);
+                kulcs = core4prog.strconst(maxhossz);
+
+                probalkozas++;
+
+                teszt(exor(kodoltszoveg, kulcs), kulcs);
+            }
+        }
+    }
+}
---
+++ xor_bruteforce/xor_bruteforce/core1prog.cs
@@ -0,0 +1,154 @@
+﻿using System;
+
+namespace xor_bruteforce
+{
+    //a többi core.cs is ugyan ez
+
+
+    static class core1prog
+    {
+        //karakterkészletek
+        public static char[] upper = new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', };
+        public static char[] lower = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', };
+        public static char[] number = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        public static char[] special = new char[] { '`', '~', '!', '@', '#', '$', '%', '^', '*', '(', ')', '_', '+', '-', '=', '{', '}', '|', '[', ']', '&', '\\', ':', '\'', ';', '\'', '<', '>', '?', ',', '.', '/' };
+
+        public static string[] egyedi = new string[] { "1", "2" };
+
+        public static int a, b, c, d, e, f, g, h, i, j = 0;
+
+        public static string[] arrayka;
+
+        //karakterkészlet összerakása
+        public static void arraybuilder(int up, int low, int num, int spec, int rev)
+        {
+            int arraylenght = 0;
+            string sztring = "";
+            if (up >= 1)
+            {
+                sztring += new string(upper);
+                arraylenght += upper.Length;
+            }
+            if (low >= 1)
+            {
+                sztring += new string(lower);
+                arraylenght += lower.Length;
+            }
+            if (num >= 1)
+            {
+                sztring += new string(number);
+                arraylenght += number.Length;
+            }
+            if (spec >= 1)
+            {
+                sztring += new string(special);
+                arraylenght += special.Length;
+            }
+
+            char[] chararray = sztring.ToCharArray();
+
+            string[] stringarray = new string[arraylenght + 1];
+            //az első elem üres
+            stringarray[0] = null;
+
+            for (int x = 1; x < arraylenght + 1; x++)
+            {
+                stringarray[x] = chararray[x - 1].ToString();
+            }
+
+            if (rev >= 1)
+                Array.Reverse(stringarray);
+
+            arrayka = stringarray;
+        }
+
+        //minimum hosszúság beállítása
+        public static void minhossz(int min)
+        {
+            if (min == 0)
+                return;
+            if (min > 0)
+                a = 1;
+            if (min > 1)
+                b = 1;
+            if (min > 2)
+                c = 1;
+            if (min > 3)
+                d = 1;
+            if (min > 4)
+                e = 1;
+            if (min > 5)
+                f = 1;
+            if (min > 6)
+                g = 1;
+            if (min > 7)
+                h = 1;
+            if (min > 8)
+                i = 1;
+            if (min > 9)
+                j = 1;
+            if (min > 10)
+                Console.WriteLine("A minimum rövidség túl nagy! A beírt érték: " + min);
+        }
+
+        //a kulcs összerakása
+        public static string strconst(int maxhossz)
+        {
+            string egesz;
+
+
+            egesz = arrayka[a] + arrayka[b] + arrayka[c] + arrayka[d] + arrayka[e] + arrayka[f] + arrayka[g] + arrayka[h] + arrayka[i] + arrayka[j];
+            if (egesz.Length > maxhossz)
+                return "Max hossz elérve!";
+
+            a++;
+
+            if (a == arrayka.Length)
+            {
+                a = 1;
+                b++;
+            }
+            if (b == arrayka.Length)
+            {
+                b = 1;
+                c++;
+            }
+            if (c == arrayka.Length)
+            {
+                c = 1;
+                d++;
+            }
+            if (d == arrayka.Length)
+            {
+                d = 1;
+                e++;
+            }
+            if (e == arrayka.Length)
+            {
+                e = 1;
+                f++;
+            }
+            if (f == arrayka.Length)
+            {
+                f = 1;
+                g++;
+            }
+            if (g == arrayka.Length)
+            {
+                g = 1;
+                h++;
+            }
+            if (h == arrayka.Length)
+            {
+                h = 1;
+                i++;
+            }
+            if (i == arrayka.Length)
+            {
+                i = 1;
+                j++;
+            }
+            return egesz;
+        }
+    }
+}
